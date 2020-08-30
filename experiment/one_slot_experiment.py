7bb3c845c09c021298fc6c7b1b0ea309fa0673e2
neardws <singleangel@vip.qq.com>
2020-08-30 19:23:48 +0800

    experiment init

---
+++ experiment/one_slot_experiment.py
@@ -0,0 +1,11 @@
+#!./venv python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   one_slot_experiment.py    
+@Contact :   neard.ws@gmail.com
+@Github  :   neardws
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2020/8/30 下午7:23   neardws      1.0         None
+"""
